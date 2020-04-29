e156d833394948eb6bb8f567ab5a7bd7432ec97b
benchaplin <bendchaplin@gmail.com>
2020-04-29 22:35:00 +0400

    returns matching as pair, rather than single string

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setuptools.setup(
 	name='hungarian_algorithm',
-	version='0.1.8',
+	version='0.1.9',
 	author='Ben Chaplin',
 	author_email='benchaplin@protonmail.ch',
 	description='Python 3 implementation of the Hungarian Algorithm for the assignment problem.',
